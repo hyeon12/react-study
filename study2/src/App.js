34e23b164a4b83c6e5a8714051cdac52dab330d8
hyeon12 <56420106+hyeon12@users.noreply.github.com>
2021-06-23 21:12:39 +0900

    188

--- study2/src/App.js
+++ study2/src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
-import IterationSample from "./IterationSample";
+import ErrorBoundary from "./ErrorBoundary";
+import LifeCycleSample from "./LifeCycleSample";
+
+//랜덤 색상을 생성합니다
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  //Math.floor() 내림 정수값-내림 후 가까운 정수를 반환
+}
 
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
   render() {
     return (
       <div>
-        <IterationSample/>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
       </div>
     );
   }
