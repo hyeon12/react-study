77db4e17db252c5e7411a18e5f6cb24244a82895
hyeon12 <56420106+hyeon12@users.noreply.github.com>
2021-09-06 22:39:08 +0900

    217

--- study3/src/App.js
+++ study3/src/App.js
@@ -1,23 +1,8 @@
 import React, { useState } from "react";
-import Counter from "./Counter";
-import Info from "./Info";
+import Average from "./Average";
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <Counter />
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr /> {/*선 생김 */}
-      {visible && <Info />}
-    </div>
-  );
+  return <Average />;
 };
 
 export default App;
